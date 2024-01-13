0ad58bc190e80441afde5e4f84dbeb3b4a6b2172
SFDingDAng <37648498+SFDingDAng@users.noreply.github.com>
2024-01-13 23:46:12 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 import re
 
-# 要删除的行
-line_to_delete = "^http[s]?:\/\/magev6\.if\.qidian\.com\/argus\/api\/v1\/bookshelf\/refresh url reject-200"
+# 要删除的行的模式
+pattern_to_delete = "^http[s]?:\/\/magev6\.if\.qidian\.com\/argus\/api\/v1\/bookshelf\/refresh url reject-200"
 
 # 要替换的字符串
 string_to_replace = "https://raw.githubusercontent.com/Yu9191/Rewrite/main/Qidian_my.js"
@@ -12,13 +12,21 @@ replacement_string = "https://raw.githubusercontent.com/SFDingDAng/chongxie/main
 # 要处理的文件名
 filename = "chongxie.txt"
 
-# 读取文件内容，并存储除了要删除的行之外的所有行
+# 读取文件内容
 with open(filename, 'r') as file:
     lines = file.readlines()
 
-# 使用正则表达式替换指定字符串
-new_lines = [re.sub(string_to_replace, replacement_string, line) if line.strip("\n") != line_to_delete else line for line in lines]
+# 处理每一行，删除或替换
+new_lines = []
+for line in lines:
+    # 检查是否是要删除的行
+    if re.match(pattern_to_delete, line):
+        continue  # 不添加到new_lines，即删除该行
+    else:
+        # 不是要删除的行，检查是否需要替换字符串
+        line = re.sub(string_to_replace, replacement_string, line)
+        new_lines.append(line)
 
-# 将新内容写回文件
+# 将修改后的内容写回文件
 with open(filename, 'w') as file:
     file.writelines(new_lines)
